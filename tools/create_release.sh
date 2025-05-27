297d3b10ffe287713d8cce6c60edf2d7fdaf368b
Hatter Jiang <jht5945@gmail.com>
2025-05-28 00:08:02 +0800

    feat: update create release

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -4,7 +4,8 @@ TAGNAME=$1
 
 DATA='{"tag_name":"'$TAGNAME'","name":"'$TAGNAME'","draft":false,"prerelease":true,"generate_release_notes":true}'
 
-curl -fsSL \
+# curl -fsSL \
+curl -L -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
